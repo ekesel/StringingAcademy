1bc34190d3495016574f9dd74c4df6ae4bb8fafd
Ekaansh Sahni <ekaansh.sahni@gmail.com>
2023-03-21 00:38:20 +0530

    update admin text

--- stringing/urls.py
+++ stringing/urls.py
@@ -30,3 +30,7 @@ urlpatterns = [
 
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
+
+admin.site.site_header = "Stringing Academy"
+admin.site.site_title = "Stringing | Admin"
+admin.site.index_title = "Admin Eke Welcomes You"
